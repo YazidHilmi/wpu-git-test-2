217408023606d1063025ec61590790ef6df2be9d
YazidHilmi <yazidhilmi10@gmail.com>
2024-07-02 00:58:20 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Helllo wold</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste, error pariatur. Similique quas illo ratione neque quam omnis corrupti laborum animi velit! Blanditiis dolorum adipisci vitae officiis culpa illo minus?</p>
 </body>
 </html>
